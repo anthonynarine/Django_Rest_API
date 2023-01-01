3dd8eea868fb50778cd05f251aae3b8fd46dbd21
anthonynarine <anarine83@gmail.com>
2023-01-01 15:04:40 -0500

    API Completed with Full CRUD Functionality + Added Postman Collection

--- countries/models.py
+++ countries/models.py
@@ -4,10 +4,18 @@ from django.db import models
 
 class Countries(models.Model):
     name = models.CharField(max_length=50, blank=False, default="")
+    
     capital = models.CharField(max_length=50, blank=False, default="")
-      
-    #django will automatically add an auto-increment integer pk column named id
-    #the subclass below will sort our db records in acending order whe a query is made
+    
+    def __str__(self):
+        return self.name
     
     class Meta:
         ordering = ("id", )
+      
+    #django automatically adds an auto-increment integer pk column named id
+    
+    #the subclass above will sort our db records in acending order whe a query is made
+    
+    
+    #DONT FORGET TO REGISTER THIS MODEL IN ADMIN.PY
--- countries/urls.py
+++ countries/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+# from django.conf.urls import url
 from . import views
 from  django.urls import path
 
@@ -7,3 +7,11 @@ urlpatterns = [
     path("<int:pk>/", views.countries_detail),
 
 ]
+
+
+# using regular expression 
+
+# urlpaterns = [
+#     url(r"^api/countries$", views.countries_list),
+#     url(r"^api/countries/(?P<pk>[0-9]+)$", views.countries_detail)
+# ]
