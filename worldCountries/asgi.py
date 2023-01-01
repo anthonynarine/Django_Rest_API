d24c850ec8bafebb46d3e38f2755eba4e8b6606b
anthonynarine <anarine83@gmail.com>
2022-12-31 20:05:04 -0500

    Created Project + Updated Setting + Moved DB and SecretKey to gitignore + Updated CORS Headers

--- worldCountries/asgi.py
+++ worldCountries/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for worldCountires project.
+ASGI config for worldCountries project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,9 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "worldCountires.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "worldCountries.settings")
 
 application = get_asgi_application()
-
-
-
--- worldCountries/urls.py
+++ worldCountries/urls.py
@@ -1,4 +1,4 @@
-"""worldCountires URL Configuration
+"""worldCountries URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
